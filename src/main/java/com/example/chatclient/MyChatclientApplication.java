790301506dd3cacfc508276add81aaf557f7edc4
biasmj016 <mjkim031@gmail.com>
2025-03-27 23:06:04 +0900

    project init

---
+++ src/main/java/com/example/chatclient/MyChatclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChatclientApplication.class, args);
+	}
+
+}
