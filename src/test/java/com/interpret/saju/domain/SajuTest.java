6d87fe4e5547c8451933d7e137a39e31a685b4e4
biasmj016 <mjkim031@gmail.com>
2025-04-15 23:45:18 +0900

    도메인 테스트 추가

---
+++ src/test/java/com/interpret/saju/domain/SajuTest.java
@@ -0,0 +1,20 @@
+package com.interpret.saju.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SajuTest {
+    @Test
+    public void getKey() {
+        Saju saju = new Saju(1990, 5, 15, 14, "남성", true, true, "홍길동", "서울");
+        String expectedKey = "1990_5_15_14_남성_true_true_홍길동_서울";
+        assertEquals(expectedKey, saju.getKey());
+    }
+    @Test
+    public void getKey_optional() {
+        Saju saju = new Saju(1990, 5, 15, 14, "남성", true, true, null, null);
+        String expectedKey = "1990_5_15_14_남성_true_true";
+        assertEquals(expectedKey, saju.getKey());
+    }
+}
